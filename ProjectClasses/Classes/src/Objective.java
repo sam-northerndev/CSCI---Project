b6cca5fc95b84ac8f3706dd443ac7825fa4a59e0
BlakePatriquin <bep121314@gmail.com>
2017-03-25 14:13:06 -0300

    Update Objective.java

--- ProjectClasses/Classes/src/Objective.java
+++ ProjectClasses/Classes/src/Objective.java
@@ -6,7 +6,7 @@ public class Objective {
 		hp = 1000;
 		
 	}
-	public boolean Destroyed() {
+	public boolean Destroyed() { // if the objective is destroyed
 		if (hp < 0) {
 			return true;
 		}
@@ -14,7 +14,7 @@ public class Objective {
 			return false;
 		}
 	}
-	public void Attacked(Character c) {
+	public void Attacked(Character c) { // loses hp if objective is attacked
 		if (c.attack) {
 			hp -= c.getattack;
 		}
