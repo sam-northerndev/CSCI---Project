045a0b62be29fd223867cbfb931f82d2aa5866fd
cr461739 <cr461739@dal.ca>
2017-03-25 14:44:13 -0300

    Added the different types of characters (Empty classes)

---
+++ ProjectClasses/Classes/src/Warrior.java
@@ -0,0 +1,10 @@
+/* This class extends the Character class to a warrior class
+ * The Warrior has high base attack stats, medium defense  and HP, and low magic and resistance
+ * Started March 25, 2017
+ */
+public class Warrior extends Character {
+	public Warrior(){
+		super(25,4,1,2,3);
+	}
+
+}
