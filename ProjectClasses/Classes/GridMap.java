ceca21da439768ab0305c7aa324af276740ae76b
Sam <samuel.irvine711@gmail.com>
2017-03-30 12:46:06 -0300

    updated grid

--- ProjectClasses/Classes/GridMap.java
+++ ProjectClasses/Classes/GridMap.java
@@ -5,28 +5,41 @@
 import java.util.LinkedList;
 import java.util.Random;
 
+import javax.xml.soap.Node;
+
 public class GridMap {
   
-	private LinkedList<EmptySpace>[][] grid = new LinkedList<Space>[1][8];
+	private LinkedList[][] grid = new LinkedList<Space>[1][8];
 	
 	public GridMap() {
+		for(int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++){
+				EmptySpace tempData = new EmptySpace();
+				EmpNode temp = new EmpNode(tempData, grid[0][i].getFirst()); //*need linked list get front..
+				grid[0][i].add(temp);
+			}
+		}
+	}
 		
 	}
 	public void generateObstacles() { //method to populate the board with obstacles
 		Random rnd = new Random();
 		int target = rnd.nextInt(10);
 		int guess;	// call linkedlist at [i], if a randomly generated number satisfies requirement place object.
-		for (int i = 0; i < grid.length; i++ ) { 
-			while (grid[1][i] != null) {
+		for (int i = 0; i < grid.length; i++ ) {
+			Node temp = grid[0][i].getFirst();//*need linked list first method
+			int count = 0; //keep track of where to place the new obstacle
+			while (temp != null) {
 				guess = rnd.nextInt(10);
-				if (guess == target) {
+				if (guess == target && temp.getClass() == EmpNode.class) { //the node is an empty space
 					Obstacles obst = new Obstacle();
-					grid[1][i].add(obst); //setting the data at [1][i] to append to the end.
+					grid[0][i].addAt(obst,count); //*setting the node to replace the node at current index
+				}
+				temp = temp.getNext(); //* get next
+				count++;
+			}
 				
-				}				 
-		}
-			
-		
+		}	
 	}
 	public void  generateObjective() {
 		
