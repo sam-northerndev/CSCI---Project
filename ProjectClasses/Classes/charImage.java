4a83bc643cca971e87dafd59c2c5fb93f28fe24b
mariah07 <mariahpinto2@gmail.com>
2017-04-15 15:27:48 -0300

    update charImage

--- ProjectClasses/Classes/charImage.java
+++ ProjectClasses/Classes/charImage.java
@@ -10,23 +10,23 @@ public class charImage {
       Image i = new Image(new File("redmage.png").toURI().toString());
       if(n.equals("R")){
          if(c.getType() == 0)
-            new Image(new File("redrogue.png").toURI().toString());
+            i = new Image(new File("redrogue.png").toURI().toString());
          else if (c.getType() == 1)
-            new Image(new File("redwarrior.png").toURI().toString());
+            i = new Image(new File("redwarrior.png").toURI().toString());
          else if (c.getType() == 2)
-            new Image(new File("redmage.png").toURI().toString());
+            i = new Image(new File("redmage.png").toURI().toString());
          else
-            new Image(new File("redtank.png").toURI().toString());
+            i = new Image(new File("redtank.png").toURI().toString());
       }
       if(n.equals("B")){
          if(c.getType() == 0)
-            new Image(new File("bluerogue.png").toURI().toString());
+            i = new Image(new File("bluerogue.png").toURI().toString());
          else if (c.getType() == 1)
-            new Image(new File("bluewarrior.png").toURI().toString());
+            i = new Image(new File("bluewarrior.png").toURI().toString());
          else if (c.getType() == 2)
-            new Image(new File("bluemage.png").toURI().toString());
+            i = new Image(new File("bluemage.png").toURI().toString());
          else
-            new Image(new File("bluetank.png").toURI().toString());
+            i = new Image(new File("bluetank.png").toURI().toString());
       }
       return i;
    }
